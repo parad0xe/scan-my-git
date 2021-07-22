284a2ecffa35b0e3b8e8ac560f269435749cb89b
Yann-IT <y.folacci@it-students.fr>
2021-07-22 16:25:15 +0200

    prepare to merge with feature/new-docker-environment

--- src/Controller/RunnerController.php
+++ src/Controller/RunnerController.php
@@ -3,19 +3,19 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
-use App\Message\RunnerMessage;
 use App\Service\ModuleManager;
 use App\Repository\RunnerRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/runner')]
 class RunnerController extends AbstractController {
     #[Route('/run/{analysis_hash}/{runner_id}', name: 'runner.run', methods: ['POST'])]
-    public function run(MessageBusInterface $bus, string $analysis_hash, int $runner_id): JsonResponse {
-        $bus->dispatch(new RunnerMessage($runner_id));
+    public function run(LoggerInterface $logger, string $analysis_hash, int $runner_id): JsonResponse {
+        $logger->info("runner {$runner_id} started.");
+        sleep(5);
+        $logger->info("runner {$runner_id} finished.");
         return new JsonResponse(['success'=> true]);
     }
 }
